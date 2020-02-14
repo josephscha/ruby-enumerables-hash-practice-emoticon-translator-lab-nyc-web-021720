e90ee532240b96e07e80253859da8f05c4e0fe7f
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 02:35:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,6 @@ require "yaml"
 def load_library(file)
   emoticons = YAML.load_file(file)
   emoticon_hash = {:get_meaning => {}, :get_emoticon => {}}
-  return emoticons
 end 
 
 def get_japanese_emoticon(file, emoticon)
