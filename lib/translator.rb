b398a378ad739cf3717da09cdf4b1d076a1f59c1
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 02:28:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ end
 def get_japanese_emoticon(emoticons, value)
   # code goes here
   library = load_library(emoticons)
-  if library
+  if library[:get_emoticon]
 end
 
 def get_english_meaning(emoticons, value)
